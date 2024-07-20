267ce3def9fb0711e109f92ffb973293a407e575
islahatun Nufusi <islahatunnufusi07@gmail.com>
2024-07-20 20:38:12 +0700

    create model dan controller

---
+++ app/Http/Controllers/LaporanPelanggaranController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Laporan_pelanggaran;
+use Illuminate\Http\Request;
+
+class LaporanPelanggaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Laporan_pelanggaran  $laporan_pelanggaran
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Laporan_pelanggaran $laporan_pelanggaran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Laporan_pelanggaran  $laporan_pelanggaran
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Laporan_pelanggaran $laporan_pelanggaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Laporan_pelanggaran  $laporan_pelanggaran
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Laporan_pelanggaran $laporan_pelanggaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Laporan_pelanggaran  $laporan_pelanggaran
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Laporan_pelanggaran $laporan_pelanggaran)
+    {
+        //
+    }
+}
