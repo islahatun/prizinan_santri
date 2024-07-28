3e18a69af1c95d22f6385314676d30e6002c9f59
islahatun Nufusi <islahatunnufusi07@gmail.com>
2024-07-28 14:44:14 +0700

    perbaikan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         // php artisan db:seed --class=SurahSeeder  -> untuk menjalankan seed surah
+          // php artisan db:seed --class=UserSeeder  -> untuk menjalankan seed surah
 
     }
 }
